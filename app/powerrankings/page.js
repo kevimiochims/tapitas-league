edd0064f7bd36f3cc70fc17a671201e94bccf7b0
Kevim Iochims <kevimiochims@MacBook-Air-de-Kevim.local>
2026-05-28 22:25:38 -0300

    update

--- app/powerrankings/page.js
+++ app/powerrankings/page.js
@@ -915,17 +915,11 @@ export default function PowerRankingsPage() {
                           {/* FORM */}
 
                           <div
-                            ref={(el) => {
-                              if (!el) return
-
-                              requestAnimationFrame(() => {
-                                el.scrollLeft = 99999
-                              })
-                            }}
                             className="mt-4 overflow-x-auto scroll-hide"
+                            dir="rtl"
                           >
 
-                            <div className="flex items-center gap-1 min-w-max">
+                            <div className="flex items-center gap-1 min-w-max" dir="ltr">
 
                               {seasonResults.map((r, idx) => (
 
