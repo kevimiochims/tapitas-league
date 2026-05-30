2d329b4c602a25faa7ea68ac25358ad8e5fe5575
Kevim Iochims <kevimiochims@MacBook-Air-de-Kevim.local>
2026-05-30 17:29:39 -0300

    update

--- app/config/draftPhotos.js
+++ app/config/draftPhotos.js
@@ -10,7 +10,24 @@ export const DRAFT_PHOTOS = {
     { file: 'foto8.jpg', caption: 'Draft finalizado com sucesso' },
   ],
   2024: [
-    { file: 'foto1.jpg', caption: 'Draft 2024' },
+    { file: 'foto1.png', caption: 'Os GMs reunidos ao final do Draft' },
+    { file: 'foto2.jpg', caption: 'Preparativos' },
+    { file: 'foto3.jpg', caption: 'Campeão e Unicornio' },
+    { file: 'foto4.jpg', caption: 'O cobiçado troféu' },
+    { file: 'foto5.png', caption: 'Hora da escolha' },
+    { file: 'foto6.jpg', caption: 'Pós Draft 1' },
+    { file: 'foto7.jpg', caption: 'Pausa para descansar' },
+    { file: 'foto8.jpg', caption: 'Draft finalizado com sucesso' },
+  ],
+  2023: [
+    { file: 'foto1.png', caption: 'Os GMs reunidos ao final do Draft' },
+    { file: 'foto2.jpg', caption: 'Preparativos' },
+    { file: 'foto3.jpg', caption: 'Campeão e Unicornio' },
+    { file: 'foto4.jpg', caption: 'O cobiçado troféu' },
+    { file: 'foto5.png', caption: 'Hora da escolha' },
+    { file: 'foto6.jpg', caption: 'Pós Draft 1' },
+    { file: 'foto7.jpg', caption: 'Pausa para descansar' },
+    { file: 'foto8.jpg', caption: 'Draft finalizado com sucesso' },
   ],
   // adicione mais anos aqui
 }
