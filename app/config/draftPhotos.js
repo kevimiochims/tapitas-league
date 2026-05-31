2c7b29687b8746af951dfbb489f0834b7cc1a655
Kevim Iochims <kevimiochims@MacBook-Air-de-Kevim.local>
2026-05-30 22:20:19 -0300

    update

--- app/config/draftPhotos.js
+++ app/config/draftPhotos.js
@@ -20,7 +20,7 @@ export const DRAFT_PHOTOS = {
     { file: 'foto8.jpg', caption: 'Draft finalizado com sucesso' },
   ],
   2023: [
-    { file: 'foto1.png', caption: 'Os GMs reunidos ao final do Draft' },
+    { file: 'foto1.jpeg', caption: 'Os GMs reunidos ao final do Draft' },
     { file: 'foto2.jpg', caption: 'Preparativos' },
     { file: 'foto3.jpg', caption: 'Campeão e Unicornio' },
     { file: 'foto4.jpg', caption: 'O cobiçado troféu' },
