d7f58c8bae1c5cee6529299f837864b5eecc9847
Kevim Iochims <kevimiochims@MacBook-Air-de-Kevim.local>
2026-09-02 16:24:44 -0300

    update

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#171717] text-white">
+      <body className="bg-[#16274F] text-white">
         <DrawerProvider>
           <MobileDrawer />
           {children}
